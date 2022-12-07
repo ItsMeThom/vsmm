238920a397ea7da5fc8d240c956bb40b7355f663
ItsMeThom <thom.lambert@outlook.com>
2022-12-07 16:05:33 +0000

    feat: Update signalling/observer to allow different signals from the same object

--- vsmm/signalling/observer.py
+++ vsmm/signalling/observer.py
@@ -1,6 +1,11 @@
 """
 Observe pattern objects for use in communicating between UI/backend
+
+Loosely based on Godots' signal design pattern.
 """
+
+
+from typing import List
 
 
 class Event:
@@ -8,16 +13,17 @@ class Event:
 
 class Observable:
 
-    def __init__(self):
-        self.callbacks = []
+    def __init__(self, signals: List[str]):
+        self.signals = signals
+        self.callbacks = {s: [] for s in self.signals}
 
-    def subscribe(self, callback):
+    def subscribe(self, signal: str, callback: function):
         self.callbacks.append(callback)
 
-    def fire(self, **attrs):
+    def emit_signal(self, signal: str, **attrs):
         e = Event()
         e.source = self
         for k, v in attrs.items():
             setattr(e, k, v)
-        for fn in self.callbacks:
+        for fn in self.callbacks[signal]:
             fn(e)
